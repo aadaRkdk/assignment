a07e9d0d48b0256b28ef14351928806b60d4494f
Aadarsha Khadka <meaadar1@gmail.com>
2024-04-23 09:00:05 +0545

    q3 solved.

--- javascript/basic/operators.js
+++ javascript/basic/operators.js
@@ -8,16 +8,18 @@ console.log(z);
 //q2. Check below code, output should be false by comparing. console.log the answer
 const a = 10
 const b = '10'
-
 console.log(a===b);
 
 //q3. generate a random number between 0 and 5, if it is lesser than 3, console.log "lesser than 3" else "Greater"
 
 const num = Math.floor(Math.random()*6);
-
+console.log("Generated number = " + num);
 if(num<3){
-    console.log("less than 3.");
+    console.log(num + " is less than 3.");
+}
+else if(num===3){
+    console.log(num + " is equal to 3.");
 }
 else{
-    console.log("greater or equal to 3.");
+    console.log(num + " is greater than 3.")
 }
