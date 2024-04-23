4bcfb13b5462733c40b5b6a7afc04d2cfed22c5b
Aadarsha Khadka <meaadar1@gmail.com>
2024-04-23 08:34:30 +0545

    q1 and q2 answers solved.

---
+++ javascript/basic/operators.js
@@ -0,0 +1,12 @@
+
+//q1. create two constants, add them assign to a new constants
+const x = 'abc';
+const y = 'def';
+const z = x + y;
+console.log(z);
+
+//q2. Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
+
+console.log(a===b);
