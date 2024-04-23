b659f22afe1918c129d050452c4b140d541f484d
Aadarsha Khadka <meaadar1@gmail.com>
2024-04-23 09:17:26 +0545

    q3 solved.

---
+++ javascript/array/level1.js
@@ -0,0 +1,17 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+console.log("the length of array = " +arr.length);
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+console.log(arr.reverse());
+
+
+///Q3 join the array and console log
+// expected output 352
+
+const newarr = arr.join("");
+console.log(newarr);
+
+
