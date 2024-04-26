2ce19b27816e40655a90a4d8b7e49eee06c34604
Aadarsha Khadka <meaadar1@gmail.com>
2024-04-26 08:21:03 +0545

    level3 q1 solved

---
+++ javascript/array/level3.js
@@ -0,0 +1,12 @@
+
+//given the array, calculate sum of odd numbers using reduce method
+const arr =[4,2,3,5]
+
+const oddsum = arr.reduce((sum, item)=>{
+    if(item%2 !== 0){
+        sum += item;
+    }
+    return sum;
+},0)
+
+console.log(oddsum);
