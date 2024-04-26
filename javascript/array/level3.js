2cd310178cd753da23cadbf19d3f07e69f9a03a9
Aadarsha Khadka <meaadar1@gmail.com>
2024-04-26 08:25:30 +0545

    Update level3.js

--- javascript/array/level3.js
+++ javascript/array/level3.js
@@ -1,12 +1,9 @@
-
-//given the array, calculate sum of odd numbers using reduce method
+//q1. given the array, calculate sum of odd numbers using reduce method
 const arr =[4,2,3,5]
-
 const oddsum = arr.reduce((sum, item)=>{
     if(item%2 !== 0){
         sum += item;
     }
     return sum;
 },0)
-
 console.log(oddsum);
