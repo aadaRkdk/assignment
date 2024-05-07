230f1e36b16b3da1b6d6cb1254aa3c49c8ea92d5
Aadarsha Khadka <meaadar1@gmail.com>
2024-05-07 17:05:07 +0545

    basic calculator made.

--- react/myapp/src/app/calculator/page.js
+++ react/myapp/src/app/calculator/page.js
@@ -1,9 +1,109 @@
-import React from 'react'
+/* 
+This calculator is made to practise different concepts while learning. It may not be fully functional.I
+*/
+"use client";
+import { useState } from "react";
 
 const calculator = () => {
-  return (
-    <div>calculator</div>
-  )
-}
+  let [num, setNum] = useState("");
 
-export default calculator
+  let [result, setResult] = useState("");
+  const digits = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
+
+  const symbols = ["AC", "⌫", "%", "/", "*", "+", "-", "=", ".", "00"];
+
+  const handleDigitsClick = (value) => {
+    setNum((prevNum) => prevNum + value);
+  };
+
+  const handleSymbolsClick = (symbol) => {
+    switch (symbol) {
+      case "AC":
+        setNum("");
+        setResult("");
+        break;
+      case "⌫":
+        setNum((prevNum) => prevNum.slice(0, -1));
+        break;
+      case "%":
+        if (num !== "") {
+          setNum((prevNum) => (parseFloat(prevNum) / 100).toString());
+        }
+        break;
+      case "/":
+      case "*":
+      case "-":
+      case "+":
+        if (num !== "") {
+          setNum((prevNum) => prevNum + symbol);
+        }
+        break;
+      case "=":
+        if (num !== "") {
+          try {
+            setResult(eval(num).toString());
+            setNum("");
+          } catch (error) {
+            setResult("Error");
+          }
+        }
+        break;
+      case ".":
+        // Append a decimal point if num is not empty and doesn't already contain a decimal point
+        if (num !== "" && !num.includes(".")) {
+          setNum((prevNum) => prevNum + ".");
+        }
+        break;
+      case "00":
+        if (num !== "") {
+          setNum((prevNum) => prevNum + "00");
+        }
+        break;
+      default:
+        break;
+    }
+  };
+
+  const generateDigits = () => {
+    return digits.map((item, index) => {
+      return (
+        <div className="p-1" key={index}>
+          <button
+            onClick={() => handleDigitsClick(item)}
+            className="flex w-10 h-10 bg-gray-400 items-center justify-center rounded"
+          >
+            {item}
+          </button>
+        </div>
+      );
+    });
+  };
+
+  const generateSymbols = () => {
+    return symbols.map((item, index) => {
+      return (
+        <div className="p-1" key={index}>
+          <button
+            onClick={() => handleSymbolsClick(item)}
+            className="flex w-10 h-10 bg-orange-300 items-center justify-center rounded"
+          >
+            {item}
+          </button>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-gray-200 border border-gray-500 p-6 mb-4 text-right text-3xl rounded-lg">
+          {num || result}
+        </div>
+        <div>{generateDigits()}</div>
+        <div>{generateSymbols()}</div>
+      </div>
+    </>
+  );
+};
+export default calculator;
--- react/myapp/src/app/page.js
+++ react/myapp/src/app/page.js
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl">This is myapp!</h1>
+    <main className="flex min-h-screen flex-col items-center p-3">
+      <h1 className="text-5xl p-2">This is myapp!</h1>
+      <h1 className="text-3xl">you can go /calculator</h1>
     </main>
   );
 }
