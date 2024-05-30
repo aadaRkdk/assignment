3b8c472f19bfa60e1b4b79feab64a6c069771317
Aadarsha Khadka <meaadar1@gmail.com>
2024-05-30 08:21:03 +0545

    index.js in backend updated

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,23 @@
-import { createServer } from "node:http";
+const express = require('express')
+const app = express()
+const port = 3000
 
-const server = createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!  \n");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(4000, "127.0.0.1", () => {
-  console.log("Listening on 127.0.0.1:3000");
-});
+
+app.get('/user', (req, res) => {
+  res.send({
+    name: "abc",
+    balance: 100000000000,
+    reward: 1111111111111111
+  })
+})
+
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
